a559bb13274899f5d1545436b0463e954a1ecc6a
tom g <thogar@noc.ac.uk>
2026-01-23 13:25:15 +0000

    update to test to work with database

--- test/map_screen_test.dart
+++ test/map_screen_test.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_amrit/database/db.dart';
+import 'package:flutter_amrit/database/db_connection.dart' as conn;
 import 'package:flutter_amrit/helpers/location/location_fetcher.dart';
 import 'package:flutter_amrit/screens/map_screen.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -15,20 +17,25 @@ class FakeLocationFetcher extends LocationFetcher {
 
 void main() {
   testWidgets('MapScreen has title', (tester) async {
-    await tester.pumpWidget(const MaterialApp(home: MapScreen()));
+    final db = AppDatabase.executor(conn.inMemoryConnection());
+    await tester.pumpWidget(MaterialApp(home: MapScreen(database: db)));
     final titleFinder = find.text('SmartTags');
     expect(titleFinder, findsOneWidget);
+    await db.close();
   });
   testWidgets('MapScreen has find my location icon', (tester) async {
-    await tester.pumpWidget(const MaterialApp(home: MapScreen()));
+    final db = AppDatabase.executor(conn.inMemoryConnection());
+    await tester.pumpWidget(MaterialApp(home: MapScreen(database: db)));
     final iconFinder = find.byIcon(Icons.my_location);
     expect(iconFinder, findsOneWidget);
+    await db.close();
   });
   testWidgets(
     'Find my location button updates co-ordinates to users location',
     (
       tester,
     ) async {
+      final db = AppDatabase.executor(conn.inMemoryConnection());
       const fakeLocation = LatLng(12.34, 56.78);
 
       // Use the test-only callback to observe when the map is centered
@@ -37,6 +44,7 @@ void main() {
       await tester.pumpWidget(
         MaterialApp(
           home: MapScreen(
+            database: db,
             // Use a fake LocationFetcher
             locationFetcher: FakeLocationFetcher(fakeLocation),
             // Check when the map is centered via the test callback
@@ -51,6 +59,7 @@ void main() {
       // Tap the icon to center the map and wait for the test callback
       await tester.tap(find.byIcon(Icons.my_location));
       await tester.pump(const Duration(milliseconds: 200));
+      await db.close();
 
       // Wait for the callback
       final center = await moved.future.timeout(const Duration(seconds: 5));
